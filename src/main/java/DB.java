8ee205f78b65e3bbe7a55640537d4708ee5f5fd3
omoshthegeek <omoshthegeek@gmail.com>
2020-01-12 18:22:01 +0300

    added the database file

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -1,2 +1,7 @@
+import org.sql2o.Sql2o;
+
 public class DB {
+    //    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/wildlife_tracker", "moringa", "@qwerty");
+    public static Sql2o sql2o = new Sql2o("jdbc:postgresql://ec2-174-129-33-176.compute-1.amazonaws.com:5432/dfq9hqlo3csq93", "moringa", "@qwerty");
+
 }
